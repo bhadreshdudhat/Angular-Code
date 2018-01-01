8fe9821832e05148dede632ee6a84d533d25d0a7
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-01-01 15:09:12 -0600

    property binding using interpolation
    binding class data with html template

--- app.js
+++ app.js
@@ -9,10 +9,11 @@
   //creating second component
   var RandomQuoteComponent = Component({
     selector: 'random-quote',
-    template: '<p>This is a quote.</p>'
+    template: '<p><em>{{quote.line}}</em> - <b>{{quote.author}}</b></p>'//Property binding with interpolation{{}}
   })
   .Class({
     constructor: function() {
+      this.quote=quotes[0];//adding a property for RandomQuoteComponentx
     }
   });
 
@@ -43,31 +44,48 @@
   //telling Angular to bootsrap App MODULE we just created
   platformBrowserDynamic().bootstrapModule(AppModule);
 
+  var quotes=[
+    {
+      "line": "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      "author": "Brian W. Kernighan"
+    },
+    {
+      "line": "Walking on water and developing software from a specification are easy if both are frozen.",
+      "author": "Edward V Berard"
+    },
+    {
+      "line": "It always takes longer than you expect, even when you take into account Hofstadter's Law.",
+      "author": "Hofstadter's Law"
+    },
+    {
+      "line": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
+      "author": "Rick Osborne"
+    },
+    {
+      "line": "In theory, there is no difference between theory and practice. But, in practice, there is.",
+      "author": "Jan L. A. van de Snepscheut"
+    },
+    {
+      "line": "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+      "author": "Bill Gates"
+    },
+    {
+      "line": "There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.",
+      "author": "Leon Bambrick"
+    },
+    {
+      "line": "Nine people can't make a baby in a month.",
+      "author": "Fred Brooks"
+    },
+    {
+      "line": "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
+      "author": "Edsger Dijkstra"
+    },
+    {
+      "line": "The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.",
+      "author": "Tom Cargill"
+    }
+  ];
+
 })();
 
-/*
-(function() {
-
-  var AppComponent = ng.core.Component({
-    selector: 'my-app',
-    template: '<h1>Hello World!</h1>'
-  })
-  .Class({
-    constructor: function() { }
-  });
-
-  var AppModule = ng.core.NgModule({
-    imports: [ng.platformBrowser.BrowserModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
-  })
-  .Class({
-    constructor: function() { }
-  });
-
-  ng.platformBrowserDynamic.platformBrowserDynamic()
-    .bootstrapModule(AppModule);
-
-})();
-
-*/
