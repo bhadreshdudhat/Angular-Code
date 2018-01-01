716fd808bcb9ab8848e1fa6b41be7d504aeafaba
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-01-01 14:33:18 -0600

    Creating Alias

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // wraaping everything in immediately invoke javascript function so that it wont't pollute global name space
 (function(){
+  //craeting alias for readability
+  var Component=ng.core.Component;
+  var NgModule=ng.core.NgModule;
+  var BrowserModule=ng.platformBrowser.BrowserModule;
+  var platformBrowserDynamic=ng.platformBrowserDynamic.platformBrowserDynamic;
+
     //creating Angular component
     //ES 5 - access all angular 2 function by ng namespace available globally 
     //we are passing object to the Component method
-    var AppComponent=ng.core.Component({
+    var AppComponent=Component({
         selector:'my-app',//metadata used by Angular
         template: '<h1>Hello World!</h1>'//html for our component
     })
@@ -13,8 +19,8 @@
     
   //Angular knows how to run our app
   //1. create angular module (ng module)
-  var AppModule = ng.core.NgModule({
-    imports: [ng.platformBrowser.BrowserModule],
+  var AppModule = NgModule({
+    imports: [BrowserModule],
     declarations: [AppComponent],//all the components in our app
     bootstrap: [AppComponent]//which will rendered in our index.html
   })
@@ -23,8 +29,7 @@
   });
 
   //telling Angular to bootsrap App MODULE we just created
-  ng.platformBrowserDynamic.platformBrowserDynamic()
-    .bootstrapModule(AppModule);
+  platformBrowserDynamic().bootstrapModule(AppModule);
 
 })();
 
