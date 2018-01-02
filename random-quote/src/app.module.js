17d95ef5adf5a279881bc7ce5339483967fff1c3
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-01-02 12:47:50 -0600

    Convering AppModule to class

--- random-quote/src/app.module.js
+++ random-quote/src/app.module.js
@@ -1,18 +1,19 @@
 (function(app) {
-  var NgModule = ng.core.NgModule;
+  var NgModule=ng.core.NgModule;
   var BrowserModule = ng.platformBrowser.BrowserModule;
   var QuoteService = app.QuoteService;
   var RandomQuoteComponent = app.RandomQuoteComponent;
   var AppComponent = app.AppComponent;
 
-  app.AppModule = NgModule({
+  @NgModule({
     imports: [BrowserModule],
     declarations: [AppComponent, RandomQuoteComponent],
     providers: [QuoteService],
     bootstrap: [AppComponent]
   })
-  .Class({
-    constructor: function() { }
-  });
+  
+  class AppModule{ }
+  
+  app.AppModule = AppModule;
 
 })(window.app || (window.app = {}));
