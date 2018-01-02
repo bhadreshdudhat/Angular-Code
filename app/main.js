f8c70304f21ba382a8d95c3b8825cf2fbc65c2e9
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-01-01 22:30:23 -0600

    Seperating service

--- app/main.js
+++ app/main.js
@@ -1,43 +1,9 @@
-(function(){
+(function(app){
   var Component=ng.core.Component;
   var NgModule=ng.core.NgModule;
   var BrowserModule=ng.platformBrowser.BrowserModule;
   var platformBrowserDynamic=ng.platformBrowserDynamic.platformBrowserDynamic;
-  var Class=ng.core.Class;
-
-  var QuoteService = Class({
-    constructor: function QuoteService() {
-      this.quotes = sampleQuotes;
-    },
-    
-    getRandomQuote: function() {
-      var randomIndex = Math.floor(Math.random() * this.quotes.length);
-      return this.quotes[randomIndex];
-    },
-
-    generateRandomQuotes: function(delay, callback) {
-      var self = this;
-      callback(this.getRandomQuote());
-      setInterval(function() {
-        callback(self.getRandomQuote());
-      }, delay);
-    }
-  });
-
-  //Dependency Injection
-// Different service for replacement
-var MockQuoteService = Class({
-  constructor: function QuoteService() {
-    this.quotes = sampleQuotes;
-  },
-  getRandomQuote: function() {
-    return {
-      line:'mock quote',
-      author:'mock auother'
-    }
-  }
-});
-
+  var QuoteService=app.QuoteService;
 
   var RandomQuoteComponent = Component({
     selector: 'random-quote',
@@ -82,49 +48,6 @@ var MockQuoteService = Class({
 
   platformBrowserDynamic().bootstrapModule(AppModule);
 
-  var sampleQuotes=[
-    {
-      "line": "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-      "author": "Brian W. Kernighan"
-    },
-    {
-      "line": "Walking on water and developing software from a specification are easy if both are frozen.",
-      "author": "Edward V Berard"
-    },
-    {
-      "line": "It always takes longer than you expect, even when you take into account Hofstadter's Law.",
-      "author": "Hofstadter's Law"
-    },
-    {
-      "line": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
-      "author": "Rick Osborne"
-    },
-    {
-      "line": "In theory, there is no difference between theory and practice. But, in practice, there is.",
-      "author": "Jan L. A. van de Snepscheut"
-    },
-    {
-      "line": "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
-      "author": "Bill Gates"
-    },
-    {
-      "line": "There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.",
-      "author": "Leon Bambrick"
-    },
-    {
-      "line": "Nine people can't make a baby in a month.",
-      "author": "Fred Brooks"
-    },
-    {
-      "line": "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
-      "author": "Edsger Dijkstra"
-    },
-    {
-      "line": "The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.",
-      "author": "Tom Cargill"
-    }
-  ];
-
-})();
+})(window.app ||(window.app={}));
 
 
